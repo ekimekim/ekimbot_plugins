87181abb20abd7802505f2c074180555f6d22dc6
Mike Lang <mikelang3000@gmail.com>
2015-01-07 01:18:01 -0800

    A plugin to make the bot say stuff

---
+++ say.py
@@ -0,0 +1,23 @@
+
+from ekimbot.botplugin import ClientPlugin, CommandHandler
+
+
+class SayCommand(ClientPlugin):
+	name = 'say'
+
+	@CommandHandler('say', 1)
+	def say(self, msg, *args):
+		self.reply(msg, ' '.join(args))
+
+	@CommandHandler('say_at', 2)
+	def say_at(self, msg, target, *args):
+		self.client.msg(target, ' '.join(args))
+
+	@CommandHandler('do', 1)
+	def do(self, msg, *args):
+		self.reply(msg, ('ACTION', ' '.join(args)))
+
+	@CommandHandler('do_at', 2)
+	def do_at(self, msg, target, *args):
+		self.client.msg(target, ('ACTION', ' '.join(args)))
+
