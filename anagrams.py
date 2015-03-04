79c580c413611d94f078065074d7b7545d3a4b58
Mike Lang <mikelang3000@gmail.com>
2015-03-04 06:46:03 -0800

    anagrams: Load async

--- anagrams.py
+++ anagrams.py
@@ -2,22 +2,33 @@
 import os
 from collections import Counter
 
+import gevent
+from gevent.event import AsyncResult
+
 from ekimbot.botplugin import ClientPlugin
 from ekimbot.commands import CommandHandler
 
-words = {}
+
+words = AsyncResult()
+# TODO logging here concerning async load
+
 def load_dict():
 	global words
-	words = {}
+	_words = {}
 	path = '/usr/share/dict'
 	for name in os.listdir(path):
 		filepath = os.path.join(path, name)
 		with open(filepath) as f:
 			lines = f.read()
-		for line in lines.strip().split('\n'):
+		for n, line in enumerate(lines.strip().split('\n')):
 			count = tuple(sorted(Counter(line.lower()).items()))
-			words.setdefault(count, set()).add(line)
-load_dict()
+			_words.setdefault(count, set()).add(line)
+			if n % 10000 == 0:
+				gevent.sleep(0.01) # let other greenlets act
+	words.set(_words)
+
+gevent.spawn(load_dict)
+
 
 class AnagramsCommand(ClientPlugin):
 	name = 'anagrams'
@@ -26,7 +37,7 @@ class AnagramsCommand(ClientPlugin):
 	def anagrams(self, msg, *args):
 		word = ' '.join(args)
 		count = tuple(sorted(Counter(word.lower()).items()))
-		matches = filter(lambda w: w.lower() != word.lower(), words.get(count, set()))
+		matches = filter(lambda w: w.lower() != word.lower(), words.get().get(count, set()))
 		if matches:
 			self.reply(msg, "Anagrams of {}: {}".format(word, ', '.join(matches)))
 		else:
