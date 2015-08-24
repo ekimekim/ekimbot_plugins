c3ee053df965994b16ed42190e1e4a7b4a35e723
Mike Lang <mikelang3000@gmail.com>
2015-08-23 21:18:01 -0700

    Add instrumentation to anagrams for testing

--- anagrams.py
+++ anagrams.py
@@ -1,5 +1,6 @@
 
 import os
+import sys
 from collections import Counter
 
 import gevent
@@ -39,8 +40,7 @@ class AnagramsCommand(ClientPlugin):
 	@CommandHandler('anagrams', 1)
 	def anagrams(self, msg, *args):
 		word = ' '.join(args)
-		count = tuple(sorted(Counter(word.lower()).items()))
-		matches = filter(lambda w: w.lower() != word.lower(), words.get().get(count, set()))
+		matches = find_anagrams(word)
 		if matches:
 			self.reply(msg, "Anagrams of {}: {}".format(word, ', '.join(matches)))
 		else:
@@ -49,3 +49,35 @@ class AnagramsCommand(ClientPlugin):
 	@CommandHandler('anagram', 1)
 	def anagram(self, msg, *args):
 		return self.anagrams(msg, *args)
+
+
+def find_anagrams(word):
+	count = tuple(sorted(Counter(word.lower()).items()))
+	matches = filter(lambda w: w.lower() != word.lower(), words.get().get(count, set()))
+	return matches
+
+
+if __name__ == '__main__':
+	from monotonic import monotonic
+	def get_mem():
+		with open('/proc/self/stat') as f:
+			values = f.read().strip().split()
+			rss = int(values[23]) * 4
+			return rss
+
+	test_words = sys.stdin.read().strip().split('\n')
+
+	info = []
+	info.append(("start", monotonic(), get_mem()))
+	load_dict()
+	info.append(("loaded", monotonic(), get_mem()))
+	for word in test_words:
+		find_anagrams(word)
+	info.append(("look up {}x".format(len(test_words)), monotonic(), get_mem()))
+
+	_, prev_t, prev_mem = info[0]
+	FORMAT = "{}: {:+.2f}s, {:+f}MB"
+	for name, t, mem in info[1:]:
+		print FORMAT.format(name, t - prev_t, (mem - prev_mem) / 1024.0)
+		prev_t, prev_mem = t, mem
+	print "{}: {:.2f}s, {:f}MB".format("final", prev_t, prev_mem / 1024.0)
