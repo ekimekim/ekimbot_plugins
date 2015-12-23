5647858fdd5bf4a5d78a23767522f48bd31eb3de
Mike Lang <mikelang3000@gmail.com>
2015-12-22 19:27:29 -0800

    Reply on "tradition" just like PyMoronBot for that hivemind effect

---
+++ tradition.py
@@ -0,0 +1,16 @@
+
+import re
+
+from girc import Handler
+from ekimbot.botplugin import ClientPlugin
+
+# we specifically want to match PyMoronBot's pattern for best consistency
+PATTERN = re.compile(r'([^a-zA-Z]|^)as is tradition([^a-zA-Z]|$)')
+
+class TraditionPlugin(ClientPlugin):
+	"""Replies 'as is tradition' in respond to 'as is tradition', as is tradition."""
+	name = 'tradition'
+
+	@Handler(command='PRIVMSG', payload=PATTERN)
+	def as_is_tradition(self, client, msg):
+		self.reply(msg, "As is tradition.")
