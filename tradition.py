bd83d7cdc41f4897b6f5dca2f8ada95f86995bc4
Mike Lang <mikelang3000@gmail.com>
2016-11-19 19:45:15 -0800

    fix missing case-insensitivity in tradition

--- tradition.py
+++ tradition.py
@@ -5,7 +5,7 @@ from girc import Handler
 from ekimbot.botplugin import ClientPlugin
 
 # we specifically want to match PyMoronBot's pattern for best consistency
-PATTERN = re.compile(r'([^a-zA-Z]|^)as is tradition([^a-zA-Z]|$)')
+PATTERN = re.compile(r'([^a-zA-Z]|^)as is tradition([^a-zA-Z]|$)', flags=re.I)
 
 class TraditionPlugin(ClientPlugin):
 	"""Replies 'as is tradition' in respond to 'as is tradition', as is tradition."""
