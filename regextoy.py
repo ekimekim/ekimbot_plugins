920775b851076e49f6661941cdc4f731f65d91e2
Mike Lang <mikelang3000@gmail.com>
2016-11-28 04:36:00 -0800

    regextoy: also allow actually testing the regex

--- regextoy.py
+++ regextoy.py
@@ -1,14 +1,16 @@
 
+import time
+
 from ekimbot.botplugin import ClientPlugin
 from ekimbot.commands import CommandHandler
 
-from dfaregex import parse, to_text, ParseError
+from dfaregex import parse, to_text, ParseError, match
 
 class RegexToy(ClientPlugin):
 	name = 'regextoy'
 
-	@CommandHandler('regextoy', 1)
-	def regextoy(self, msg, *args):
+	@CommandHandler('regexformat', 1)
+	def regexformat(self, msg, *args):
 		args = ' '.join(args)
 		if not args:
 			return
@@ -22,3 +24,18 @@ class RegexToy(ClientPlugin):
 				self.reply(msg, "sorry, regex too long. avoid '.' or large character ranges.")
 			else:
 				self.reply(msg, "an equivalent regex: {}".format(text))
+
+	@CommandHandler('regextest', 2)
+	def regextest(self, msg, pattern, *args):
+		if not args:
+			return
+		text = ' '.join(args)
+		start = time.time()
+		try:
+			result = match(pattern, text)
+		except ParseError as e:
+			self.reply(msg, "bad regex: {}".format(e))
+		else:
+			result = 'matches' if result else 'does not match'
+			latency = time.time() - start
+			self.reply(msg, "{} (took {:.2f}ms)".format(result, latency * 1000.))
