d0d9218e11f4bb4bd3d0ed035161c47ffccb904b
Mike Lang <mikelang3000@gmail.com>
2017-01-30 11:40:05 -0800

    whoislive: Allow arbitary 'follows of this user' in the args

--- whoislive.py
+++ whoislive.py
@@ -1,4 +1,6 @@
 
+import itertools
+
 import gevent
 import gtools
 import twitch
@@ -27,7 +29,7 @@ class WhoIsLive(ClientPlugin):
 
 	defaults = {
 		'target': None, # None makes us use client.nick
-		'limit': 5,
+		'limit': 3,
 	}
 
 	@property
@@ -39,12 +41,23 @@ class WhoIsLive(ClientPlugin):
 
 	@CommandHandler("live", 0)
 	def whoislive(self, msg, *channels):
-		"""List all currently live streamers from follow list"""
+		"""List currently live streamers
+
+		Specify list of channels, or list of all channels followed by a channel by prepending a ~
+		If nothing given, a default follow list is used depending on bot config
+		"""
 		found = []
 		errors = False
+		if not channels:
+			channels = [self.target]
 		try:
-			if not channels:
-				channels = self.following()
+			# flatten iterators of follows and direct channel names into single iterable
+			# TODO this could be better parallelised so follow fetches happen in parallel
+			# but we need to refactor to use gevent queues or it gets real ugly real fast
+			channels = itertools.chain(*[
+				self.following(channel.lstrip('~')) if channel.startswith('~') else (channel,)
+				for channel in channels
+			])
 			for name, channel in gtools.gmap_unordered(self.get_channel_if_live, channels):
 				if not channel:
 					continue
@@ -62,9 +75,9 @@ class WhoIsLive(ClientPlugin):
 		elif not found:
 			self.reply(msg, "No-one is live right now, sorry!")
 
-	def following(self):
-		"""Yields channel names that self.target is following"""
-		for result in self.api.get_all("follows", "users", self.target, "follows", "channels"):
+	def following(self, target):
+		"""Yields channel names that target is following"""
+		for result in self.api.get_all("follows", "users", target, "follows", "channels"):
 			yield encode_recursive(result['channel']['name'])
 
 	def get_channel_if_live(self, name):
