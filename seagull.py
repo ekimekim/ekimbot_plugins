970d26e708c30031f5a1ed83485095ed882df6e2
Mike Lang <mikelang3000@gmail.com>
2017-03-09 05:04:22 -0800

    seagull: Add ability to specify target length, count unicode chars if able

--- seagull.py
+++ seagull.py
@@ -8,17 +8,34 @@ class SeagullCommand(ClientPlugin):
 	name = 'seagull'
 
 	@CommandHandler('seagull', 0)
-	def seagull(self, msg, openchar='{', closechar='}', *args):
+	def seagull(self, msg, openchar='{', closechar='}', targetlen=20, *args):
 		if openchar == closechar:
 			self.reply(msg, "Can't make a flock out of only one character")
 			return
+		try:
+			targetlen = int(targetlen)
+		except ValueError:
+			self.reply(msg, "Invalid length: {!r}".format(targetlen))
+			return
+		# opportunistically measure length in unicode chars if both strings valid unicode
+		try:
+			_openchar = openchar.decode('utf-8')
+			_closechar = closechar.decode('utf-8')
+		except UnicodeDecodeError:
+			self.logger.info("Got invalid unicode in one of {!r}, {!r}".format(openchar, closechar))
+		else:
+			openchar = _openchar
+			closechar = _closechar
+			self.logger.info("Using opportunistic unicode handling: {!r}, {!r}".format(openchar, closechar))
 		out = openchar
 		depth = 1
 		while depth:
-			c = openchar if random.random() * 20 > len(out) else closechar
+			c = openchar if random.random() * targetlen > len(out) else closechar
 			if c == openchar:
 				depth += 1
 			else:
 				depth -= 1
 			out += c
+		if isinstance(out, unicode):
+			out = out.encode('utf-8')
 		self.reply(msg, out)
