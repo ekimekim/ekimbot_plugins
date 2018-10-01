8d35e9ab67c22f1144220ae63786262808e6140b
Mike Lang <mikelang3000@gmail.com>
2018-10-01 03:31:10 -0700

    uptime: Remove buggy process start code that doesn't work on some systems due to missing ps features

--- uptime.py
+++ uptime.py
@@ -1,6 +1,4 @@
 
-import os
-import subprocess
 import time
 
 from ekimbot.botplugin import ClientPlugin
@@ -13,24 +11,14 @@ import_time = time.time()
 class Uptime(ClientPlugin):
 	name = 'uptime'
 
-	_process_start_time = None
-
 	def init(self):
 		self.client_start_time = time.time()
-
-	@classmethod
-	def get_process_start_time(cls):
-		if cls._process_start_time is None:
-			# I don't want to do a ps every time, this introduces some uncertainty but close enough
-			cls._process_start_time = time.time() - float(subprocess.check_output(['ps', '-o', 'etimes=', str(os.getpid())]))
-		return cls._process_start_time
 
 	@CommandHandler('uptime', 0)
 	def uptime(self, msg, *args):
 		now = time.time()
-		self.reply(msg, "{} since process start, {} since interpreter start, {} since client start".format(
+		self.reply(msg, "{} since interpreter start, {} since client start".format(
 			*map(self.format_interval, [
-				now - self.get_process_start_time(),
 				now - import_time,
 				now - self.client_start_time,
 			])
